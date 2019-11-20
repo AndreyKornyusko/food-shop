ca253713330f42aa5393e71ae43acbd0d927bfca
Andrey Kornyushko <kap86@ukr.net>
2019-11-20 15:08:02 +0200

    fixed loader

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import styles from './Loader.module.scss';
 
-const Loader = () => (
-  <div className={styles.loader}>
-    Loading... (Сервер на бесплатном аккаунте Heroku и может находиться в режиме
-    сна в течении 30 сек после запроса.)
-  </div>
-);
+const Loader = () => <div className={styles.loader}>Loading...</div>;
 
 export default Loader;
